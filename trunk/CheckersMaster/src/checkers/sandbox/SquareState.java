385ffbf307852200c9654a43dc7a9770f1fbe07c
ykaragol <ykaragol@641b9e76-3628-11de-a996-7ff70a941223>
2009-05-10 19:35:54 +0000

    enum ayrıldı. daha da ayırsak ne?

---
+++ trunk/CheckersMaster/src/checkers/sandbox/SquareState.java
@@ -0,0 +1,15 @@
+package checkers.sandbox;
+
+public enum SquareState{
+	BLANK,
+	WHITE,
+	BLACK,
+	KING_WHITE,
+	KING_BLACK
+}
+	// 0 means blank
+	// 1 means white
+	// 2 means black
+	// 3 means king white
+	// 4 means king black
+

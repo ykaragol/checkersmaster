dc681b8e101018faf1786efb5a3bd7f98f9c927d
ykaragol <ykaragol@641b9e76-3628-11de-a996-7ff70a941223>
2009-05-24 23:22:27 +0000

    ...

--- CheckersMaster/src/checkers/domain/Move.java
+++ CheckersMaster/src/checkers/domain/Move.java
@@ -1,7 +1,16 @@
 package checkers.domain;
 
+import checkers.sandbox.SquareState;
+
 public class Move {
-	private int value;
+	private int value = Integer.MIN_VALUE;
+	public int fromX;
+	public int fromY;
+	public int toX;
+	public int toY;
+	public boolean must = false;
+	public SquareState eat;
+	public Move next;
 
 	public int getValue() {
 		return value;
